3e9a649618eaf58113fd8106d48a00f2f4ef0dcc
ag_dubs <ashley666ashley@gmail.com>
2015-07-15 14:17:06 -0400

    add webmaker android login script and desc

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 
 ## webmaker-desktop (READONLY)
 A user arriving from the snippet lands on the `/discover` page. They then navigate to a specific project; next, they visit a related project. Finally they return to the `/discover` page.
+
+## webmaker-android (LOGIN)
+A pre-existing user enters their username and password. A POST request is made to `/login/oauth/access_token`.
---
+++ webmaker-android_LOGIN.lua
@@ -0,0 +1,25 @@
+-- webmaker-android user story (LOGIN-ONLY)
+-- summary: request access token
+
+-- user agent: android version >= 41
+http.set_user_agent_string("Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0")
+
+local base_url = "https://id-webmaker-org-staging.herokuapp.com"
+
+local client_id = "client_id=wm_id_zIPGbkEDB5Cv9dCzo7nS"
+local grant_type = "&&grant_type=password"
+local uid = "&&uid=agdubs"
+local password = "&&password=lolololologoff"
+local scopes = "&&scopes=projects"
+
+local post_data = client_id .. grant_type .. uid .. password .. scopes
+
+-- request access token from id
+http.request_batch({
+    {
+    "POST",
+      base_url .. "/login/oauth/access_token",
+      data=post_data
+    }
+})
+
